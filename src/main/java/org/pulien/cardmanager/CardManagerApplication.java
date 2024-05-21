d5d8d7e01c425d4270a235cd8d931c37d46af46b
theoahga <clere.theo@gmail.com>
2024-05-21 14:40:54 +0200

    feat : init spring web server

---
+++ src/main/java/org/pulien/cardmanager/CardManagerApplication.java
@@ -0,0 +1,13 @@
+package org.pulien.cardmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CardManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pulien/cardmanager/CardManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pulien.cardmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
