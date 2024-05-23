5d50e16dd12bb91300adeaa7275c79c9ea88e95e
Ceïf <cecece775@gmail.com>
2024-05-23 16:42:57 +0200

    merge of main

---
+++ front/src/components/CardPreview.tsx
@@ -0,0 +1,9 @@
+const CardPreview = ()=> {
+    return <div className="h-screen overflow-hidden rounded-lg flex justify-center items-center p-4">
+        <div className="rounded-md w-64 h-96 bg-red-700">
+
+        </div>
+    </div>
+}
+
+export default CardPreview;
--- front/src/pages/CardPage.tsx
+++ front/src/pages/CardPage.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { useNavigate } from "react-router-dom";
-
+import InventoryTable from "@/components/Table";
+import CardPreview from "@/components/CardPreview";
 const CardPage = () =>{
     const navigate = useNavigate();
 
@@ -10,10 +11,14 @@ const CardPage = () =>{
     }
 
     return(
-        <>
-            <Button onClick={logout}>Logout</Button>
-            <div>CardPage</div>
-        </>
+        <div className=" w-screen bg-slate-700 grid grid-cols-3 gap-4">
+            <div className="col-span-2 bg-green-500 h-full px-2">
+                <InventoryTable />
+            </div>
+            <div className="col-span-1 bg-blue-500 h-full px-2 ">
+                <CardPreview />
+            </div>
+        </div>   
     )
 }
 
